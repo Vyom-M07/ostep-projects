d67331b7ae9f66753ee4d5998f9bce1b38e8994e
Vyom-M07 <vyom.manot.539@gmail.com>
2026-08-14 03:09:42 -0500

    need to figure out how to incorporate piping into this unix command

---
+++ initial-utilities/wgrep/wgrep.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  // argv[0] is ./wgrep
+  // argv[1] is the word we are looking for
+  // argv[2...] are all the files to look inside
+  // printf("arg count: %d\n", argc);
+  // printf("argv 0: %s\n", argv[0]);
+  // printf("argv 1: %s", argv[1]);
+  if (argc == 1) {
+    // if someone types in just './wgrep'
+    printf("wgrep: searchterm [file ...]\n");
+    exit(1);
+  }
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+  char* keyword = argv[1];
+  // printf("keyword: %s\n", keyword);
+  for (int i = 1; i < argc - 1; i++) {
+    FILE* fp = fopen(argv[i + 1], "r");
+    if (fp == NULL) {
+      printf("wgrep: cannot open file\n");
+      exit(1);
+    }
+    while ((nread = getline(&line, &len, fp)) != -1) {
+      // printf("readline: %s\n", line); line contains the line read from the
+      // file now we have to find the keyword within this line (.contains() type
+      // vibe)
+      if (strstr(line, keyword) != NULL) {
+        printf("%s", line);
+      }
+    }
+  }
+
+  return 0;
+}
